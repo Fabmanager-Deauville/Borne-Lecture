ca3fc448052d9efe5b388b6ac15c18feadb36712
MaximeNssh <50406678+MaximeNssh@users.noreply.github.com>
2019-05-12 16:24:12 +0200

    Activation touches a,z,e
    Création d'affichage dans la console à chaque touche préssée (a,z,e)

--- js/borneAudio.js
+++ js/borneAudio.js
@@ -1,1 +1,17 @@
-alert("hello");
+// 4- click au CLAVIER
+window.onkeyup = function(e) {
+
+   switch (e.keyCode) {
+     case 65:
+        console.log("la touche a est appuyée");
+       break;
+      case 90:
+        console.log("la touche z est appuyée");
+       break;
+     default:
+     case 69:
+        console.log("la touche e est appuyée");
+       break;
+
+   }
+}
